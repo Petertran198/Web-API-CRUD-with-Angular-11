876016788b245cbc07bc5e3b0b5241717b2bdc6f
petertran98 <Petertran198@gmail.com>
2022-06-13 11:04:37 -0700

    added service declaration and model

--- PaymentApp/src/app/app.module.ts
+++ PaymentApp/src/app/app.module.ts
@@ -4,11 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PaymentDetailsComponent } from './payment-details/payment-details.component';
 import { PaymentDetailFormComponent } from './payment-details/payment-detail-form/payment-detail-form.component';
+import { PaymentDetailService } from './shared/payment-detail.service';
 
 @NgModule({
-  declarations: [AppComponent, PaymentDetailsComponent, PaymentDetailFormComponent],
+  declarations: [
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailFormComponent,
+  ],
   imports: [BrowserModule],
-  providers: [],
+  providers: [PaymentDetailService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
